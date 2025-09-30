5e33383fabb6166dcc4bf5d773294e80596186dc
Xurenjun <m18643986388@gmail.com>
2025-09-30 15:15:42 +0900

    exerciseのAPI動くように実装

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,14 +1,40 @@
 package main
 
 import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/labstack/echo/v4"
-	"net/http"
+	rest_exercise "github.com/xurenjun0806/workout-log/backend/interfaces/rest/exercise"
+	"github.com/xurenjun0806/workout-log/backend/interfaces/rest/middleware"
+	usecase_exercise "github.com/xurenjun0806/workout-log/backend/usecase/exercise"
+)
+
+const (
+	defaultTimeout = 30
+	defaultAddress = ":9090"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	timeoutStr := os.Getenv("CONTEXT_TIMEOUT")
+	timeout, err := strconv.Atoi(timeoutStr)
+	if err != nil {
+		log.Println("failed to parse timeout, using default timeout")
+		timeout = defaultTimeout
+	}
+	timeoutContext := time.Duration(timeout) * time.Second
+	e.Use(middleware.SetRequestContextWithTimeout(timeoutContext))
+
+	// TODO: useCaseはいったんダミーで
+	rest_exercise.NewExerciseHandler(e, &usecase_exercise.UseCase{})
+
+	// Start Server
+	address := os.Getenv("SERVER_ADDRESS")
+	if address == "" {
+		address = defaultAddress
+	}
+	log.Fatal(e.Start(address))
 }
